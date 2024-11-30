1c23c73866c8059dc646ee8c5995c19c5b3c8fff
Greashajain <greashaj@gmail.com>
2024-11-30 23:33:54 +0530

    Adding files

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
 
-        myLinkedList.printAll();
+        System.out.println("LL before reverse():");
+        myLinkedList.printList();
+
+        myLinkedList.reverse();
+
+        System.out.println("\nLL after reverse():");
+        myLinkedList.printList();
 
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            Head: 4
-            Tail: 4
-            Length: 1
-
-            Linked List:
+            LL before reverse():
+            1
+            2
+            3
             4
+
+            LL after reverse():
+            4
+            3
+            2
+            1
 
         */
 
